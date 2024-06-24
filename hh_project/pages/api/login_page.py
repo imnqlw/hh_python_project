d299d83f08a27077e95937fdfd5e1e6532fe4eda
Elmin Gulamov <g.elmn@yandex.ru>
2024-06-24 09:34:20 +0400

    fix something

--- hh_project/pages/api/login_page.py
+++ hh_project/pages/api/login_page.py
@@ -1,11 +1,11 @@
 from hh_project.utils.api_helper import api_request
-from tests.api.conftest import base_url, name, passw
+from tests.api.conftest import url, name, passw
 
 
 class LoginPage:
 
     def login_user(self):
-        result = api_request(url=base_url, endpoint='/account/login', method='POST', params=(
+        result = api_request(url=url, endpoint='/account/login', method='POST', params=(
             {'username': name, 'password': passw, 'q': 'backurl=%2F'}))
         assert result.status_code == 200
         assert 'force_login' in result.text
--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,10 +1,16 @@
 import os
 from dotenv import load_dotenv
+import pytest
 
-
-base_url = 'https://hh.ru/'
+url = os.getenv('url')
 name = os.getenv('name')
 passw = os.getenv('passw')
 text = os.getenv('text')
 wrong_pass = os.getenv('wrong_pass')
 load_dotenv()
+
+
+@pytest.fixture()
+def base_api_url():
+    base_url = url
+    return base_url
